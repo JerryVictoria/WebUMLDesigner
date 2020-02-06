b5b98ac5f7be380308a4c06692cbc0a61c57183f
shirting <1251448362@qq.com>
2020-02-06 14:13:54 +0800

    整合Designer界面

---
+++ src/views/designer/ComponentsStoreSideBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        component
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "components-store-side-bar"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/designer/ToolTopBar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        tooltopbar
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "tool-top-bar"
+    }
+</script>
+
+<style scoped>
+
+</style>
